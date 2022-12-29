ba8daeeaaf70b97e7f7caaea7fdfcccdeab8e28b
Carolina943 <akamifox943@gmail.com>
2022-12-29 14:16:29 -0400

    modified and new files

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -6,6 +6,11 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
+//rest of package
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+
+
 //database
 const connectDB = require('./db/connect');
 
@@ -13,11 +18,17 @@ const connectDB = require('./db/connect');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+
 //routes
 const authRouter = require('./routes/authRoutes');
 
+app.use(morgan('tiny'));
+app.use(express.json());
+app.use(cookieParser(process.env.JWT_SECRET));
+
 
 app.use('/api/v1/auth', authRouter);
+
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
