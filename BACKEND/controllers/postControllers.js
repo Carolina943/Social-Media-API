224a43e59177389f42fd3b16843eaa07449efd45
Carolina943 <akamifox943@gmail.com>
2023-01-03 14:56:10 -0400

    modified file

--- BACKEND/controllers/postControllers.js
+++ BACKEND/controllers/postControllers.js
@@ -1,20 +1,39 @@
 const Post = require('../models/Post');
+const {StatusCodes} = require('http-status-codes');
+const CustomError = require('../errors');
+const path = require('path');
 
 
 const createPost = async (req,res) =>{
-  res.send('Create Post');
+   req.body.user = req.user.userId;
+   const post = await Post.create(req.body);
+   res.status(StatusCodes.CREATED).json({post});
 };
 
 const getAllPost = async (req, res) =>{
-res.send('Get All Post');
+  const post = await Post.find({});
+  res.status(StatusCodes.OK).json({post, count: post.length});
 };
 
 const getSinglePost = async (req, res) =>{
- res.send('Get Single Post');
+   const {id: postId} = req.params;
+   const post = await Post.findOne({_id: postId});
+   if(!post){
+     throw new CustomError.NotFoundError(`No post with id: ${postId}`);
+   }
+   res.status(StatusCodes.OK).json({post});
 }; 
 
 const updatePost = async (req,res) =>{
-   res.send('Update Post');
+   const {id: postId} = req.params;
+   const post = await Post.findOneAndUpdate({_id: postId}, req.body, {
+      new: true,
+      runValidators: true,
+   });
+   if(!post){
+     throw new CustomError.NotFoundError(`No post with id: ${postId}`);
+   };
+  res.status(StatusCodes.OK).json({post});
 };
 
 const deletePost = async (req, res)=>{
