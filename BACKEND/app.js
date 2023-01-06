4a596cc8290adb728d47d58c5b17f460202bd4ee
Carolina943 <akamifox943@gmail.com>
2023-01-05 20:04:43 -0400

    modified app.js

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -11,6 +11,10 @@ const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const fileUpload = require('express-fileupload');
+const rateLimiter = require('express-rate-limit');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const mongoSanitize = require('express-mongo-sanitize');
 
 //database
 const connectDB = require('./db/connect');
@@ -23,6 +27,7 @@ const followRouter = require('./routes/followRoutes');
 const postRouter = require('./routes/postRoutes');
 const replyRouter = require('./routes/replyRoutes');
 
+
 //middleware
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
@@ -30,7 +35,20 @@ const errorHandlerMiddleware = require('./middleware/error-handler');
 
 
 app.use(morgan('tiny'));
+
+app.set('trust proxy', 1);
+app.use(
+   rateLimiter({
+     windowMs: 15*60*1000,
+     max: 60,
+   })
+);
+
+app.use(helmet());
 app.use(cors());
+app.use(xss());
+app.use(mongoSanitize());
+
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
 
